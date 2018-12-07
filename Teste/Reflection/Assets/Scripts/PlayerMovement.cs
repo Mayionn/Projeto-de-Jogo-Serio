2c9f64784f935b5bb1d36638f368d2663e4b0341
Débora FM <debofm@gmail.com>
2018-12-07 14:48:53 +0000

    peixe

---
+++ Teste/Reflection/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour {
+    public Animator animator;
+    public GameObject reflection;
+    [SerializeField]
+    private float speed;
+    public GameObject doorPlayer;
+    public Sprite winDoor;
+    public Sprite normalDoor;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Vector3 inputp = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        transform.position = transform.position + inputp * speed* Time.deltaTime;
+
+        Vector3 inputr = new Vector2(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical"));
+
+        reflection.transform.position = reflection.transform.position + inputr * speed * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            Die();
+        }
+
+        if (collision.gameObject.CompareTag("DoorPlayer"))
+        {
+            doorPlayer.GetComponent<SpriteRenderer>().sprite = winDoor;
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("DoorPlayer"))
+        {
+            doorPlayer.GetComponent<SpriteRenderer>().sprite = normalDoor;
+        }
+    }
+
+
+    private void Die ()
+    {
+
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+
+
+}
---
+++ Teste/Reflection/Assets/Scripts/ReflectAction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReflectAction : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Death"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+
+        Application.LoadLevel(Application.loadedLevel);
+
+    }
+}
