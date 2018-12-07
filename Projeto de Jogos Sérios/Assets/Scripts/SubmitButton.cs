5d51c3041d176629fa158e45348401dc7246e0a6
Mayionn <43456657+Mayionn@users.noreply.github.com>
2018-12-07 14:42:31 +0000

    Update correct & incorrect answers

---
+++ Projeto de Jogos Sérios/Assets/Scripts/SubmitButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SubmitButton : MonoBehaviour {
+    public string[] respostas;                      //Comparar strings aos objectos disponíveis na scene. Cria um array do tamanho desejado com os objetos.
+
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
