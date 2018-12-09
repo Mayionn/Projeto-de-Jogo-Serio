3c745f86ae5087f8a35471dcbc9ae4b3f759c5e3
Mayionn <43456657+Mayionn@users.noreply.github.com>
2018-12-09 01:32:22 +0000

    Submit button
    Respostas corretas e incorretas aparecem depois de clicar no botão submit

--- Projeto de Jogos Sérios/Assets/Scripts/SubmitButton.cs
+++ Projeto de Jogos Sérios/Assets/Scripts/SubmitButton.cs
@@ -3,13 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class SubmitButton : MonoBehaviour {
-    public string[] respostas;                      //Comparar strings aos objectos disponíveis na scene. Cria um array do tamanho desejado com os objetos.
 
-	// Update is called once per frame
-	void Update ()
+    // Array que contém os gameobjects que quisermos
+    public GameObject[] correct = { };
+    public GameObject[] incorrect = { };
+
+    // Array do texto que vai conter as respostas corretas e as incorretas
+    public GameObject[] text = { };
+    
+    // Se o primeiro objeto do array estiver ativo, então o texto também vai estar ativo
+    void Update ()
     {
-	
-	}
-}
+         if (correct[0].activeInHierarchy)
+         {
+             text[0].SetActive(true);
+         } else
+         {
+            text[0].SetActive(false);
+         }
+
+         if (correct[1].activeInHierarchy)
+         {
+             text[1].SetActive(true);
+         } else
+         {
+            text[1].SetActive(false);
+         }
+
+         if (incorrect[0].activeInHierarchy)
+         {
+            text[2].SetActive(true);
+         } else
+         {
+            text[2].SetActive(false);
+         }
+
+     // Se algum texto do array das respostas incorretas estiver visível, um botão a dizer "tentar outra vez" irá aparecer.
+     // Caso não, um botão a dizer "continuar" vai aparecer, onde nos vai levar para o próximo nível
+
+    }
+}         
+
--- Projeto de Jogos Sérios/Assets/Scripts/VisibleOrInvisibleOnClick.cs
+++ Projeto de Jogos Sérios/Assets/Scripts/VisibleOrInvisibleOnClick.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 
 public class VisibleOrInvisibleOnClick : MonoBehaviour {
 
+    private Transform originalPosition;
+
     public GameObject prefab;
     public Button m_button;
 
@@ -15,14 +17,14 @@ public class VisibleOrInvisibleOnClick : MonoBehaviour {
 
     void TaskOnClick()
     {
-
             if (prefab.activeInHierarchy)
             {
                 prefab.SetActive(false);
+            Vector3 currentPosition = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+            Quaternion currentRotation = new Quaternion (transform.rotation.x, transform.rotation.y, transform.rotation.z, 0);
             } else
             {
                 prefab.SetActive(true);
             }
-        
     }
 }
