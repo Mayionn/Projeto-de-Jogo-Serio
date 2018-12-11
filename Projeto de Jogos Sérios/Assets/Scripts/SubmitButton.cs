fc9f24112bfedad4fc4b70c2ff4a2cf60f1bc5af
Mayionn <43456657+Mayionn@users.noreply.github.com>
2018-12-11 11:07:44 +0000

    Update botão continuar e tentar outra vez
    Jogo está 99% funcional

--- Projeto de Jogos Sérios/Assets/Scripts/SubmitButton.cs
+++ Projeto de Jogos Sérios/Assets/Scripts/SubmitButton.cs
@@ -5,6 +5,13 @@ using UnityEngine.UI;
 
 public class SubmitButton : MonoBehaviour {
 
+    // Painel do Botão Submit
+    public GameObject panel;
+
+    // Game Objects para os botões
+    public GameObject tryAgain;
+    public GameObject continueGame;
+
     // Array que contém os gameobjects que quisermos
     public GameObject[] correct = { };
     public GameObject[] incorrect = { };
@@ -12,10 +19,17 @@ public class SubmitButton : MonoBehaviour {
     // Array do texto que vai conter as respostas corretas e as incorretas
     public GameObject[] text = { };
     
-    // Se o primeiro objeto do array estiver ativo, então o texto também vai estar ativo
     void Update ()
     {
-         if (correct[0].activeInHierarchy)
+        // Se o painel estiver ativo, os botões também irão estar. Isto resolve o problema dos botões aparecerem por baixo das tabs e ficarem inacessíveis
+        if (panel.activeSelf)
+        {
+            tryAgain.SetActive(true);
+            continueGame.SetActive(true);
+        } 
+
+        // Se o primeiro objeto do array estiver ativo, então o texto também vai estar ativo
+        if (correct[0].activeInHierarchy)
          {
              text[0].SetActive(true);
          } else
@@ -39,9 +53,22 @@ public class SubmitButton : MonoBehaviour {
             text[2].SetActive(false);
          }
 
-     // Se algum texto do array das respostas incorretas estiver visível, um botão a dizer "tentar outra vez" irá aparecer.
-     // Caso não, um botão a dizer "continuar" vai aparecer, onde nos vai levar para o próximo nível
-
+        // Se algum texto do array das respostas incorretas estiver visível, um botão a dizer "tentar outra vez" irá aparecer.
+        // Caso não, um botão a dizer "continuar" vai aparecer, onde nos vai levar para o próximo nível
+        if (text[2].activeInHierarchy)                                          // Texto "Tentar outra vez"
+        {
+            tryAgain.SetActive(true);
+            continueGame.SetActive(false);
+        } 
+        if (text[0].activeInHierarchy && text[1].activeInHierarchy)             // Texto "Continuar"
+        {
+            continueGame.SetActive(true);
+            tryAgain.SetActive(false);
+        } else
+        {
+            tryAgain.SetActive(true);
+            continueGame.SetActive(false);
+        }
     }
 }         
 
